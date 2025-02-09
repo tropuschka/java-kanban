51efc5bda427cfebd91ce46707fdd77bd0392037
Tropuschka <tryan.u@mail.ru>
2025-02-09 20:39:25 +0300

    1. Добавить директорию .idea в gitignore 2. Перенести класс Node в InMemoryHistoryManager 3. Удалить переменную taskInHistory 4. Исправить логику функций получения, записи и удаления истории

--- src/managing/InMemoryHistoryManager.java
+++ src/managing/InMemoryHistoryManager.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private HashMap<Integer, Task> historyFootprints = new HashMap<>();
-    private HashMap<Integer, Node<Task>> linkedHistory = new HashMap<>();
-    private Node<Task> head;
-    private Node<Task> tail;
+    private HashMap<Integer, Node> linkedHistory = new HashMap<>();
+    private Node head;
+    private Node tail;
 
     public ArrayList<Task> getHistory() {
         ArrayList<Task> history = new ArrayList<>();
-        for (Node<Task> node : linkedHistory.values()) {
-            Task task = node.getData();
-            history.add(historyFootprints.get(task.getId()));
-        }
+        Node currentNode = head;
+        do {
+            history.add(currentNode.data);
+            currentNode = currentNode.next;
+        } while (currentNode.next != null);
         return history;
     }
 
@@ -24,16 +24,18 @@ public class InMemoryHistoryManager implements HistoryManager {
     public void add(Task task) {
         if (task != null) {
             Task historyTask = task.createHistoryExample();
-            Node<Task> taskNode;
-            historyFootprints.put(task.getId(), historyTask);
+            Node taskNode;
 
             if (linkedHistory.containsKey(task.getId())) remove(task.getId());
 
             ArrayList<Task> currentHistory = getHistory();
-            if (currentHistory.size() > 0) {
-                Integer lastTaskId = currentHistory.getLast().getId();
-                taskNode = new Node<>(historyTask, linkedHistory.get(lastTaskId));
-            } else taskNode = new Node<>(historyTask, null);
+            if (!currentHistory.isEmpty()) {
+                taskNode = new Node(historyTask, tail);
+            } else {
+                taskNode = new Node(historyTask, null);
+                head = taskNode;
+            }
+            tail = taskNode;
 
             linkedHistory.put(task.getId(), taskNode);
         }
@@ -41,8 +43,28 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void remove(int id) {
-        Node<Task> toRemove = linkedHistory.get(id);
+        Node toRemove = linkedHistory.get(id);
         toRemove.removeNode();
         linkedHistory.remove(id);
     }
+
+    class Node {
+        private Node prev;
+        private Task data;
+        private Node next;
+
+        public Node(Task data, Node prev) {
+            this.prev = prev;
+            this.data = data;
+            this.next = null;
+        }
+
+        public void removeNode() {
+            Node targetPrev = prev;
+            Node targetNext = next;
+            if (targetNext != null) targetNext = targetPrev;
+            if (targetPrev != null) targetPrev = targetNext;
+        }
+    }
 }
+
