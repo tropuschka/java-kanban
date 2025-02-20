5885597c16d8a1635da13f42dfc5484f3204cbd1
Tropuschka <tryan.u@mail.ru>
2025-02-20 21:25:26 +0300

    1. Добавить тесты для записи тасков в FileBackedTaskManager 2. Удалить неиспользуемые импорты в InMemoryTaskManagerTest

---
+++ test/managing/FileBackedTaskManagerTest.java
@@ -0,0 +1,52 @@
+package managing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import taskmodels.Epic;
+import taskmodels.Subtask;
+import taskmodels.Task;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileBackedTaskManagerTest {
+    TaskManager fileBackedTM;
+
+    @BeforeEach
+    void createManager() {
+        fileBackedTM = new FileBackedTaskManager();
+    }
+
+    @Test
+    void createTask() {
+        Task task = new Task(1, "Task", "Some task");
+        fileBackedTM.createTask(task);
+        ArrayList taskArray = new ArrayList();
+        taskArray.add(task);
+        assertEquals(taskArray, fileBackedTM.getAllTasks());
+    }
+
+    @Test
+    void createEpic() {
+        Epic task = new Epic(1, "Task", "Some task");
+        fileBackedTM.createEpic(task);
+        ArrayList taskArray = new ArrayList();
+        taskArray.add(task);
+        assertEquals(taskArray, fileBackedTM.getAllEpic());
+    }
+
+    @Test
+    void createSubtask() {
+        Epic task = new Epic(1, "Task", "Some task");
+        fileBackedTM.createEpic(task);
+        Subtask subtask = new Subtask(2, "Subtask", "Some task", 1);
+        fileBackedTM.createSubtask(subtask);
+        ArrayList taskArray = new ArrayList();
+        taskArray.add(subtask);
+        assertEquals(taskArray, fileBackedTM.getAllSubtasks());
+    }
+
+    @Test
+    void downloadTMFromFile() {
+
+    }
+}
