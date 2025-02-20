0e8b504e7ed8366dfdde28a26fa7c8ea1a8a8186
Tropuschka <tryan.u@mail.ru>
2025-02-20 19:16:06 +0300

    1. Добавить сохранение тасков в файл для класса FileBackedTaskManager.

--- src/managing/FileBackedTaskManager.java
+++ src/managing/FileBackedTaskManager.java
@@ -4,26 +4,44 @@ import taskmodels.Epic;
 import taskmodels.Subtask;
 import taskmodels.Task;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class FileBackedTaskManager extends InMemoryTaskManager {
     public void save() {
-
+        try (FileWriter fw = new FileWriter("tasks.txt", StandardCharsets.UTF_8)) {
+            for (Task task : super.getAllTasks()) {
+                String taskString = task.toString();
+                fw.write(taskString + "\n\n");
+            }
+            for (Epic epic : super.getAllEpic()) {
+                String epicString = epic.getWithSubs();
+                fw.write(epicString + "\n\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи файла");
+        }
     }
 
     @Override
-    public void createTask(Task task) {
+    public Task createTask(Task task) {
         super.createTask(task);
         save();
+        return super.findTaskById(task.getId());
     }
 
     @Override
-    public void createEpic(Epic epic) {
+    public Epic createEpic(Epic epic) {
         super.createEpic(epic);
         save();
+        return super.findEpicById(epic.getId());
     }
 
     @Override
-    public void createSubtask(Subtask subtask) {
+    public Subtask createSubtask(Subtask subtask) {
         super.createSubtask(subtask);
         save();
+        return super.findSubtaskById(subtask.getId());
     }
 }
