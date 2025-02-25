9ea6d87352f0668b4212aabe6fbdb27f12b7827b
Tropuschka <tryan.u@mail.ru>
2025-02-25 18:27:46 +0300

    Заменить локальный файл на временный

--- test/managing/FileBackedTaskManagerTest.java
+++ test/managing/FileBackedTaskManagerTest.java
@@ -1,5 +1,8 @@
 package managing;
 
+import exceptions.ManagerSaveException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import taskmodels.Epic;
@@ -7,6 +10,9 @@ import taskmodels.Subtask;
 import taskmodels.Task;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -14,10 +20,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FileBackedTaskManagerTest {
     TaskManager fileBackedTM;
+    File file;
 
     @BeforeEach
-    void createManager() {
+    void createManager() throws IOException {
         fileBackedTM = new FileBackedTaskManager();
+        FileBackedTaskManager tempTM = new FileBackedTaskManager();
+        Task taskT = new Task(1, "Task", "Some task");
+        Epic epicT = new Epic(1, "Task", "Some task");
+        Subtask subtaskT = new Subtask(1, "Task", "Some task", 1);
+        tempTM.createTask(taskT);
+        tempTM.createEpic(epicT);
+        tempTM.createSubtask(subtaskT);
+        file = File.createTempFile("tasks", ".txt");
+        try (FileWriter fw = new FileWriter(file, StandardCharsets.UTF_8)) {
+            fw.write("id,type,name,status,description,epic\n");
+            for (Task task : tempTM.getAllTasks()) {
+                String taskString = task.toFile();
+                fw.write(taskString + "\n");
+            }
+            for (Epic epic : tempTM.getAllEpic()) {
+                String epicString = epic.toFile();
+                fw.write(epicString + "\n");
+            }
+            for (Subtask subtask : tempTM.getAllSubtasks()) {
+                String epicString = subtask.toFile();
+                fw.write(epicString + "\n");
+            }
+        } catch (IOException e) {
+            throw new ManagerSaveException("Ошибка записи файла");
+        }
     }
 
     @Test
@@ -28,11 +60,14 @@ public class FileBackedTaskManagerTest {
         fileBackedTM.createTask(task);
         fileBackedTM.createEpic(epic);
         fileBackedTM.createSubtask(subtask);
-        Path path = Paths.get("C:\\Users\\Galya\\Desktop\\Yandex\\java-kanban\\tasks.txt");
-        File file = path.toFile();
         FileBackedTaskManager taskManagerFromFile = FileBackedTaskManager.loadFromFile(file);
         assertTrue(fileBackedTM.getAllTasks().equals(taskManagerFromFile.getAllTasks()) &&
                 fileBackedTM.getAllEpic().equals(taskManagerFromFile.getAllEpic()) &&
                 fileBackedTM.getAllSubtasks().equals(taskManagerFromFile.getAllSubtasks()));
+    }
+
+    @AfterEach
+    void deleteTemp() {
+        file.deleteOnExit();
     }
 }
