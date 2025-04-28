9c048da176c6421cdfabcf575de99ce9d8006a19
Tropuschka <tryan.u@mail.ru>
2025-04-28 22:29:02 +0300

    Добавить метода sendText в BaseHttpHandler

--- src/httphandlers/BaseHttpHandler.java
+++ src/httphandlers/BaseHttpHandler.java
@@ -5,9 +5,18 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class BaseHttpHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+    }
+
+    protected void sendText(HttpExchange exchange, String text) throws IOException {
+        byte[] response = text.getBytes(StandardCharsets.UTF_8);
+        exchange.getRequestHeaders().add("Content-Type", "application/json;charset=utf-8");
+        exchange.sendResponseHeaders(200, response.length);
+        exchange.getResponseBody().write(response);
+        exchange.close();
     }
 }
