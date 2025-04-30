a6d641de9e3429c785ae2b67428d235d0cc6e1d1
Tropuschka <tryan.u@mail.ru>
2025-04-30 19:51:27 +0300

    Создать обработчики (шаблоны) и обновить контекст

--- src/HttpTaskServer.java
+++ src/HttpTaskServer.java
@@ -1,7 +1,6 @@
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-import httphandlers.BaseHttpHandler;
-import httphandlers.TaskHttpHandler;
+import httphandlers.*;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -12,10 +11,10 @@ public class HttpTaskServer {
     public static void main(String[] args) throws IOException {
         server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/task", new TaskHttpHandler());
-        server.createContext("/epic", new BaseHttpHandler()); //Заменить обработчик
-        server.createContext("/subtask", new BaseHttpHandler()); //Заменить обработчик
-        server.createContext("/history", new BaseHttpHandler()); //Заменить обработчик
-        server.createContext("/prioritized", new BaseHttpHandler()); //Заменить обработчик
+        server.createContext("/epic", new EpicHttpHandler());
+        server.createContext("/subtask", new SubtaskHttpHandler());
+        server.createContext("/history", new HistoryHttpHandler());
+        server.createContext("/prioritized", new PriorityHttpHandler());
 
         server.start();
     }
---
+++ src/httphandlers/HistoryHttpHandler.java
@@ -0,0 +1,29 @@
+package httphandlers;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class HistoryHttpHandler  extends BaseHttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        InputStream input = exchange.getRequestBody();
+        String method = exchange.getRequestMethod();
+        String body = new String(input.readAllBytes(), StandardCharsets.UTF_8);
+        String[] bodyArray = body.split("/");
+
+        String response;
+        switch (method) {
+            case "POST":
+                response = "post";
+            case "GET":
+                response = "get";
+            case "DELETE":
+                response = "delete";
+            default:
+                sendNotFound(exchange, "Not Found");
+        }
+    }
+}
