a30bd6afa8bbc11a92a4d1885a64080354064382
Tropuschka <tryan.u@mail.ru>
2025-04-30 22:31:48 +0300

    Обновить шаблон обработки запроса

--- src/httphandlers/TaskHttpHandler.java
+++ src/httphandlers/TaskHttpHandler.java
@@ -3,6 +3,7 @@ package httphandlers;
 import com.sun.net.httpserver.HttpExchange;
 import managing.Managers;
 import managing.TaskManager;
+import taskmodels.Task;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,14 +21,34 @@ public class TaskHttpHandler extends BaseHttpHandler {
         String body = new String(input.readAllBytes(), StandardCharsets.UTF_8);
         String[] bodyArray = body.split("/");
 
-        String response;
         switch (method) {
             case "POST":
-                response = "post";
+                if (bodyArray[1].equals("task") && bodyArray.length == 2) { // Обработать параметры
+                    manager.createTask(new Task(1, "name", "descr"));
+                    sendText(exchange, "Task \"Name\" created");
+                } else if (bodyArray[1].equals("task") && bodyArray.length == 3 && isNumber(bodyArray[2])) {
+                    int taskId = Integer.parseInt(bodyArray[2]);
+                    manager.updateTask(new Task(taskId, "name", "descr"));
+                    sendText(exchange, "Task \"Name\" updated");
+                } else sendNotFound(exchange, "Not Found");
             case "GET":
-                response = "get";
+                if (bodyArray[1].equals("task") && bodyArray.length == 2) { // Обработать параметры
+                    manager.getAllTasks();
+                    sendText(exchange, "All tasks");
+                } else if (bodyArray[1].equals("task") && bodyArray.length == 3 && isNumber(bodyArray[2])) {
+                    int taskId = Integer.parseInt(bodyArray[2]);
+                    manager.findTaskById(taskId);
+                    sendText(exchange, "Task");
+                } else sendNotFound(exchange, "Not Found");
             case "DELETE":
-                response = "delete";
+                if (bodyArray[1].equals("task") && bodyArray.length == 2) { // Обработать параметры
+                    manager.deleteAllTasks();
+                    sendText(exchange, "All tasks deleted");
+                } else if (bodyArray[1].equals("task") && bodyArray.length == 3 && isNumber(bodyArray[2])) {
+                    int taskId = Integer.parseInt(bodyArray[2]);
+                    manager.deleteTask(taskId);
+                    sendText(exchange, "Task \"Name\" deleted");
+                } else sendNotFound(exchange, "Not Found");
             default:
                 sendNotFound(exchange, "Not Found");
         }
