d0ccb0c4ca474bed6e74b2bb1f4fcdf7df3fbfe1
Tropuschka <tryan.u@mail.ru>
2025-05-03 17:17:27 +0300

    Удалить лишние методы. Почему-то менеджер не живет долго

--- src/HttpTaskServer.java
+++ src/HttpTaskServer.java
@@ -16,7 +16,7 @@ import java.net.Socket;
 
 public class HttpTaskServer {
     private static HttpServer server;
-    private static TaskManager manager = Managers.createTaskManager();
+    private static final TaskManager manager = Managers.createTaskManager();
 
 
     public static void main(String[] args) throws Exception {
@@ -24,27 +24,7 @@ public class HttpTaskServer {
         server.createContext("/", new BaseHttpHandler());
         server.createContext("/stop", new StopHandler());
         server.createContext("/task", new TaskHttpHandler(manager));
-        server.setExecutor(null); // creates a default executor
         server.start();
-
-
-//        if (hostAvailabilityCheck()) System.out.println("Server run");
-//        else System.out.println("It's dead");
-
-        // server.stop(100000000);
-
-
-    }
-
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "Hello, Galya!";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
     }
 
     static class StopHandler implements HttpHandler { //странно работает, но работает
@@ -58,14 +38,4 @@ public class HttpTaskServer {
             server.stop(100);
         }
     }
-
-/*    public static boolean hostAvailabilityCheck() {
-        try (Socket s = new Socket(String.valueOf(server.getAddress()), 8000)) {
-            return true;
-        } catch (IOException ex) {
-            /* ignore
-        }
-        return false;
-    } */
-
 }
