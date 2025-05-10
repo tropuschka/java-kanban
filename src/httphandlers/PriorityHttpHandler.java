6ab3073b08dcef700bc8df4b55f9b36e6393331e
Tropuschka <tryan.u@mail.ru>
2025-05-11 01:38:22 +0300

    Обновить обработчики для истории и списка по времени исполнения

--- src/httphandlers/PriorityHttpHandler.java
+++ src/httphandlers/PriorityHttpHandler.java
@@ -1,11 +1,13 @@
 package httphandlers;
 
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import managing.TaskManager;
 import taskmodels.Task;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class PriorityHttpHandler  extends BaseHttpHandler {
@@ -16,16 +18,20 @@ public class PriorityHttpHandler  extends BaseHttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
+        Gson gson = new Gson();
 
         switch (method) {
             case "GET":
-                ArrayList<Task> allTasksArray = manager.getPrioritizedTasks();
-                if (allTasksArray.isEmpty()) sendNotFound(exchange, "Not Found");
-                String response = allTasksArray.stream().map(Task::toString).collect(Collectors.joining("\n"));
-                sendText(exchange, response);
+                List<Task> allTasksArray = manager.getPrioritizedTasks();
+                if (!allTasksArray.isEmpty()) {
+                    String response = gson.toJson(allTasksArray.stream()
+                            .map(Task::toString)
+                            .collect(Collectors.joining("\n")));
+                    sendText(exchange, response);
+                }
                 break;
             default:
-                sendNotFound(exchange, "Not Found");
+                sendNotFound(exchange);
         }
     }
 }
